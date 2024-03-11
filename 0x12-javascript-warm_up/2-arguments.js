57742ed70190f3f965dc48404f8fe6432359a6a3
Nmbewe <nancymbewe5@gmail.com>
2024-03-11 11:24:47 -0700

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
+
+const argaLength = process.argv.length;
+
+if (argv.length === 2) {
   console.log('No argument');
-} else if (process.argv.length === 3) {
+} else if (argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
