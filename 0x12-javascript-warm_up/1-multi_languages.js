0e64a2055295db200c3a1d267f4518b75834d2ec
Nmbewe <nancymbewe5@gmail.com>
2024-03-11 11:17:16 -0700

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -3,6 +3,6 @@
 const firstLine = 'c is fun';
 const secondLine = 'python is cool';
 const thirdLine = 'javascript is amazing';
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+console.log(firstLine)
+console.log(secondLine)
+console.log(thirdLine)
